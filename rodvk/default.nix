e8c9b0dfecaf806cbd19112e6cc4917d5949c30b
Mika Tammi <mikatammi@gmail.com>
2024-01-24 23:26:32 +0200

    Vulkan Hello World
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- rodvk/default.nix
+++ rodvk/default.nix
@@ -3,12 +3,26 @@
 # SPDX-License-Identifier: MIT
 #
 # rodvk package
-{rustPlatform}:
+{
+  darwin,
+  lib,
+  rustPlatform,
+  stdenv,
+}:
 rustPlatform.buildRustPackage {
   pname = "rodvk";
   version = "0.1.0";
 
   src = ./.;
 
+  buildInputs = lib.optionals stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      AppKit
+    ]
+  );
+
+  postInstall = ''
+    ln -s ${darwin.moltenvk}/lib/libMoltenVK.dylib $out/bin/libMoltenVK.dylib
+  '';
   cargoLock.lockFile = ./Cargo.lock;
 }
--- rodvk/src/main.rs
+++ rodvk/src/main.rs
@@ -3,5 +3,21 @@
 // SPDX-License-Identifier: MIT
 
 fn main() {
-    println!("Hello, world!");
+    use vulkano::instance::{Instance, InstanceCreateInfo};
+    use vulkano::swapchain::Surface;
+    use vulkano::VulkanLibrary;
+    use winit::event_loop::EventLoop;
+
+    // let event_loop = EventLoop::new().expect("failed to create event loop"); // ignore this for now
+
+    let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+    // let required_extensions = Surface::required_extensions(&event_loop);
+    let instance = Instance::new(
+        library,
+        InstanceCreateInfo {
+            // enabled_extensions: required_extensions,
+            ..Default::default()
+        },
+    )
+    .expect("failed to create instance");
 }
